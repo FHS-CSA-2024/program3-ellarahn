3572ff7ef0ca4aa568d9c1e8d34ec9ab696d8d15
ellarahn <ellarahn040@gmail.com>
2024-08-13 09:40:08 -0700

    Update Program3.java
    took user input to store and calculate given variables to determine perimeter and area

--- Program3.java
+++ Program3.java
@@ -3,12 +3,33 @@
 import java.util.Scanner; //This imports the console scanner that reads user input
 
 //Your code here:
-
-
-
-
-//Paste console output below:
-/*
-
-
-*/
+public class Program3 {
+     public static void main(String[]args){
+     
+     // make a scanner
+         
+     Scanner myScanner = new Scanner(System.in);
+     //Create variables
+     
+     int length = 0;
+     int width = 0;
+     int perimeter = 0;
+     int area = 0;
+     
+     //ask for input
+     
+     System.out.println("Enter length");
+     length = myScanner.nextInt();
+     
+     System.out.println("Enter width");
+     width = myScanner.nextInt();
+     
+     area = length * width;
+     perimeter = length + length + width + width;
+     
+     
+     //output
+     System.out.println(length);
+     System.out.println(width);
+     System.out.println("area = " + area);
+     System.out.println("perimeter = " + perimeter);
